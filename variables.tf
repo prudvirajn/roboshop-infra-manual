bb36ce50219c4f2856c8e44b984dc0f26e95a10a
Rajprudvin <prudvirajn@gmail.com>
2024-12-09 09:26:02 +0530

    roboshop

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "instances" {
+  type        = map
+  default     = {
+    mongodb = "t3.small"
+    redis = "t3.micro"
+    mysql = "t3.small"
+    rabbitmq = "t3.micro"
+    catalogue = "t3.micro"
+    user = "t3.micro"
+    cart = "t3.micro"
+    shipping = "t3.small"
+    payment = "t3.micro"
+    dispatch = "t3.micro"
+    frontend = "t3.micro"
+  }
+}
+
+variable "allow_all" {
+    type = string
+    default = "sg-0856f58788d55001f"
+}
+
+variable "zone_id" {
+    default = "Z05949382GRHZL6CDPYX7"
+}
+
+variable "domain_name" {
+    default = "prudviraj.online"
+}
